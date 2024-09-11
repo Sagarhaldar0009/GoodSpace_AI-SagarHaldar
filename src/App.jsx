abeffa6161a2afe4831d666e84c11bb0e02dc63c
Sagarhaldar0009 <sagarhaldar987@gmail.com>
2024-09-11 23:47:20 +0530

    Footer is Added in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.scss'
 import HeroSection from './components/HeroSection/HeroSection'
-// import { NavBar } from '../components/NavBar'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="app-container">
       <HeroSection/>
+      <Footer/>
     </div>
   )
 }
