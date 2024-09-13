8db9eb7cf288a15c4b3a9dffd0f533d4f76394c1
Sagarhaldar0009 <sagarhaldar987@gmail.com>
2024-09-14 01:51:34 +0530

    Features Component is Added Successfully

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import HeroSection from './components/HeroSection/HeroSection'
 import Footer from './components/Footer/Footer'
 import Below from './components/Below/below'
 import Feedback from './components/Feedback/Feedback'
+import Features from './components/Features/Features'
 
 
 function App() {
   return (
     <div className="app-container">
       <HeroSection/>
+      <Features/>
       <div className='big-screen'>
         <Feedback/>
         <Below/> 
